3c8d9e3d583e52f9760e992305ae251709d93343
245967906 <245967906@qq.com>
2017-05-08 12:16:32 +0800

    修复某些非关键性bug (#5)

--- litchi/settings.sample.py
+++ litchi/settings.sample.py
@@ -134,6 +134,6 @@ STATICFILES_DIRS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.example.com'
 EMAIL_PORT = 25
-EMAIL_HOST_USER = 'username@.example.com'
+EMAIL_HOST_USER = 'username@example.com'
 EMAIL_HOST_PASSWORD = 'password'
 EMAIL_USE_TLS = False
