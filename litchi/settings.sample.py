7d4065e0a6dde0e10ad765cc03c5a2ab4ba0c979
245967906 <245967906@qq.com>
2017-07-02 12:45:22 +0800

    调整静态资源与模板页面目录结构

--- litchi/settings.sample.py
+++ litchi/settings.sample.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'litchi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'template'),],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
